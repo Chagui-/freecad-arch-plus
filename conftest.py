c572817ed8d3e42becaf718a94b162450423f177
Andres Peñaloza <apeciro@gmail.com>
2026-08-16 12:46:12 -0400

    refactor: move pytest conftest to the repo root

--- conftest.py
+++ conftest.py
@@ -16,7 +16,7 @@ import sys
 import types
 
 # Make the add-on modules (in the repo root) importable.
-_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+_ROOT = os.path.dirname(os.path.abspath(__file__))
 if _ROOT not in sys.path:
     sys.path.insert(0, _ROOT)
 
